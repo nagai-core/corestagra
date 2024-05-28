add723358c7d9456acbb49db3c46f865ae312e43
morita-coretech <k.morita@core-tech.jp>
2024-05-28 17:54:44 +0900

    会員登録フォーム、確認画面

---
+++ resources/views/signUp/confirm.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>登録確認</title>
+</head>
+<body>
+    <ul>
+        <li>
+            ニックネーム：
+            <p>{{$request->name}}</p>
+        </li>
+        <li>
+            ニックネーム：
+            <p>{{$request->email}}</p>
+        </li>
+        <li>
+            ニックネーム：
+            <p>{{$request->password}}</p>
+        </li>
+    </ul>
+    </body>
+</html>
