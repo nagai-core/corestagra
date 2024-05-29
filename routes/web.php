25cf8be0a0017310eb34a7da5bc7c890454eabec
nagai-core <s.nagai@core-tech.jp>
2024-05-29 15:11:31 +0900

    ログイン後indexニリダイレクト完了

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ImageController::class, 'index'])->name('index');
 Route::post('/upload', [ImageController::class, 'upload'])->name('upload');
 
-
-
-
 // Route::get("/signUp", [UserController::class, 'index'])->name('signUp.index');
 // Route::post("/confirm", [UserController::class, 'show'])->name('signUp.create');
 // Route::post("/complete", [UserController::class, 'store'])->name('signUp.store');
